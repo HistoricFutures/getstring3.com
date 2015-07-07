4c6fac73f94a5a19da81a595c418ed8c66b12293
J-P Stacey <work@jpstacey.info>
2015-07-07 12:21:55 +0100

    Refactor DatabaseTest to handle databases in setUp/tearDown

--- tests/TestRig/Services/DatabaseTest.php
+++ tests/TestRig/Services/DatabaseTest.php
@@ -13,6 +13,27 @@ use TestRig\Services\Database;
  */
 class DatabaseTest extends \PHPUnit_Framework_TestCase
 {
+
+    // Path to temporary database file.
+    private $path = NULL;
+
+    /**
+     * Set up.
+     */
+    public function setUp()
+    {
+        $this->path = "/tmp/testrig-" . getmypid() . ".sqlite3";
+        Database::create($this->path);
+    }
+
+    /**
+     * Tear down.
+     */
+    public function tearDown()
+    {
+        unlink($this->path);
+    }
+
     /**
      * Test: TestRig\Services\Database:create().
      */
@@ -22,14 +43,12 @@ class DatabaseTest extends \PHPUnit_Framework_TestCase
         {
             $this->fail("php5-sqlite must be installed via PECL, apt-get etc.");
         }
-        $path = "/tmp/testrig-" . getmypid() . "-create.sqlite3";
-        Database::create($path);
 
         // Check database even exists.
-        $this->assertFileExists($path);
+        $this->assertFileExists($this->path);
 
         // Now connect to it and look for the entity table.
-        $conn = Database::getConn($path);
+        $conn = Database::getConn($this->path);
         try
         {
             $conn->exec("SELECT * FROM entity");
@@ -40,11 +59,8 @@ class DatabaseTest extends \PHPUnit_Framework_TestCase
             {
                 $this->fail("Creating database doesn't create table entity.");
             }
-            unlink($path);
             throw $e;
         }
-
-        unlink($path);
     }
 
     /**
@@ -52,24 +68,18 @@ class DatabaseTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetTableCount()
     {
-        $path = "/tmp/testrig-" . getmypid() . "-gettablecount.sqlite3";
-        Database::create($path);
-
-        $this->assertEquals(Database::getTableCount($path, "entity"), 0);
+        $this->assertEquals(Database::getTableCount($this->path, "entity"), 0);
         try
         {
-            Database::getTableCount($path, "not_a_table");
+            Database::getTableCount($this->path, "not_a_table");
         }
         catch (Exception $e)
         {
             if (strpos($e->getMessage(), "no such table: not_a_table") === FALSE)
             {
-                unlink($path);
                 $this->fail("Can get table count from a table that doesn't exist.");
             }
         }
-
-        unlink($path);
     }
 
     /**
@@ -77,19 +87,14 @@ class DatabaseTest extends \PHPUnit_Framework_TestCase
      */
     public function testWriteRecord()
     {
-        $path = "/tmp/testrig-" . getmypid() . "-writerecord.sqlite3";
-        Database::create($path);
-
         // Write record and ensure we get an ID back.
         $record = array("name" => "Test " . uniqid());
-        Database::writeRecord($path, "entity", $record);
+        Database::writeRecord($this->path, "entity", $record);
         $this->assertEquals(1, $record["id"]);
 
         // Get the row from the database and check names match.
-        $results = Database::getConn($path)->query("SELECT * FROM entity WHERE id = 1");
+        $results = Database::getConn($this->path)->query("SELECT * FROM entity WHERE id = 1");
         $row = $results->fetchArray();
         $this->assertEquals($record["name"], $row["name"]);
-
-        unlink($path);
     }
 }
