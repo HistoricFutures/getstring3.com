317b7975ae77e2ca3bb81ed5730182d661c4a6bf
J-P Stacey <work@jpstacey.info>
2015-07-21 19:58:29 +0100

    Log expanded to include both acknowledgement and answer, and keep track better of internal clock.

--- tests/TestRig/Models/LogTest.php
+++ tests/TestRig/Models/LogTest.php
@@ -28,18 +28,29 @@ class LogTest extends \PHPUnit_Framework_TestCase
     {
         // Queue up a couple of log items.
         $this->model->logInteraction("A", "B", 50);
-        $this->model->logInteraction("C", "D", 20);
+        $this->model->logInteraction("C", "D", 20, 15);
 
         // Now check they're logged properly, with incrementing time.
         $log = $this->model->getLog();
         $this->assertEquals("A", $log[0]["from"]);
         $this->assertEquals("B", $log[0]["to"]);
         $this->assertEquals(0, $log[0]["start"]);
-        $this->assertEquals(50, $log[0]["end"]);
+        $this->assertEquals(50, $log[0]["ack"]);
         $this->assertEquals("C", $log[1]["from"]);
         $this->assertEquals("D", $log[1]["to"]);
         $this->assertEquals(50, $log[1]["start"]);
-        $this->assertEquals(70, $log[1]["end"]);
+        $this->assertEquals(70, $log[1]["ack"]);
+        $this->assertEquals(85, $log[1]["answer"]);
+    }
+
+    /**
+     * Test:: TestRig\Models\Log::timePasses().
+     */
+    public function testTimePasses()
+    {
+        $this->assertEquals(0, $this->model->timePasses(0));
+        $this->assertEquals(10, $this->model->timePasses(10));
+        $this->assertEquals(20, $this->model->timePasses(10));
     }
 
     /**
