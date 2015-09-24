06a80f6cbf1e6e3932624b0b6207f650f4a7348b
Rachel King <rachel@mrandmrsok.com>
2015-09-24 14:28:26 +0100

    Pricing start

--- assets/jquery/jquery.main.js
+++ assets/jquery/jquery.main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
         // Side drawer
         $( ".main-nav" ).clone().insertAfter( ".header" ).addClass("side-nav");
-        
+
         $( ".menu-toggle" ).click(function() {
 
           $( ".header" ).toggleClass( "open" );
@@ -26,7 +26,7 @@ $( document ).ready(function() {
          };
          previousScroll = currentScroll;
       });
-    
+
       // Same for on mobile (when scroll area is scrolling)
       $("#scroll-area").scroll(function(){
         var currentScroll = $(this).scrollTop();
@@ -43,9 +43,9 @@ $( document ).ready(function() {
          previousScroll = currentScroll;
       });
     });
-      
+
       //SVG fallback
-    
+
       $( document ).ready(function() {
           if (!Modernizr.svg) {
     var imgs = document.getElementsByTagName('img');
@@ -59,3 +59,64 @@ $( document ).ready(function() {
     }
 }
         });
+
+
+/// Compare plans
+
+$(document).ready(function() {
+      //SETUP
+      // remove expanded class from toggleable rows
+    	$( '.compare-plans .expanded' ).removeClass('expanded');
+      // hide description
+      $( '.compare-plans .show-hide' ).hide();
+      // remove rowspan attribute
+      $( '.compare-plans .toggle' ).closest('th').removeAttr('rowspan');
+
+      //ONCLICK
+      $( '.compare-plans .toggle' ).on( 'click', function () {
+        // Toggle expanded class on rows
+        $( this ).closest('tr').toggleClass( 'expanded' );
+        // Toggle rowspan attribute
+        if ($( this ).closest('th').attr('rowspan')) {
+            $( this ).closest('th').removeAttr('rowspan');
+        } else {
+            $( this ).closest('th').attr( 'rowspan','2' );
+        }
+        // Toggle description hiding and showing
+        $( this ).closest('tr').next('.show-hide').toggle();
+    });
+
+});
+
+/// Expandable list
+
+$(document).ready(function() {
+      //SETUP
+      // remove expanded class from toggleable rows
+    	$( '.expandable-list .expanded' ).removeClass('expanded');
+      // hide description
+      $( '.expandable-list .show-hide' ).hide();
+      // remove expanded class from toggleable rows
+    	$( '.expandable-single-item' ).removeClass('expanded');
+      // hide description
+      $(  '.expandable-single-item .show-hide' ).hide();
+
+
+      //ONCLICK
+      $( '.expandable-list .toggle').on( 'click', function () {
+        // Toggle expanded class on list
+        $( this ).closest('li').toggleClass( 'expanded' );
+
+        // Toggle description hiding and showing
+        $( this ).next('.show-hide').slideToggle();
+    });
+
+    $( '.expandable-single-item .toggle').on( 'click', function () {
+      // Toggle expanded class on list
+      $( this ).closest('.expandable-single-item').toggleClass( 'expanded' );
+
+      // Toggle description hiding and showing
+      $( this ).next('.show-hide').slideToggle();
+  });
+
+});
